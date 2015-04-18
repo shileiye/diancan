41547e67c90c6db215578b56aef5d480c93c960b
shileiye <shileiye@qq.com>
2015-04-18 17:38:19 +0800

    修改数据库连接

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
 //数据连接
-$con = mysql_connect("localhost","root","root");
+$con = mysql_connect("10.4.12.173","uYy2CW73bLT29","pYjulV5VB5dCk");
 if (!$con)
 {
 	die('数据连接出错: ' . mysql_error());
 }
-mysql_select_db("dc", $con);
+mysql_select_db("d22d95f26d16b4162aa180f94cca4b541", $con);
 ?>
